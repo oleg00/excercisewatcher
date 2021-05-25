7f90eb6a4f2d99b60ccacf891dd1225c5348774e
Hladkko <hladkky@gmail.com>
2021-05-25 23:49:06 +0300

    build opencv

--- src/App.java
+++ src/App.java
@@ -1,5 +1,29 @@
-public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+import java.io.IOException;
+
+import org.opencv.core.Core;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class App extends Application {
+    @Override
+    public void start(final Stage primaryStage) {
+        try {
+            Parent root = FXMLLoader.load(getClass().getResource("ui.fxml"));
+            primaryStage.setTitle("Lab 2");
+            primaryStage.setScene(new Scene(root, 300, 550));
+			primaryStage.setResizable(false);
+            primaryStage.show();
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+ 
+    public static void main(String[] args) {
+        System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
+        launch(args);
     }
 }
---
+++ src/Controller.java
@@ -0,0 +1,165 @@
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
+import org.opencv.core.Mat;
+import org.opencv.imgproc.Imgproc;
+import org.opencv.videoio.VideoCapture;
+
+import openCVutils.*;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
+public class Controller {
+
+    @FXML
+    private ImageView currentFrame;
+
+    @FXML
+    private Button button;
+
+
+    private VideoCapture capture = new VideoCapture();
+	private ScheduledExecutorService timer;
+    private boolean cameraActive = false;
+    private static int cameraId = 0;
+
+    @FXML
+    void initialize() {
+        System.out.println("loaded");
+    }
+
+    @FXML
+    void onStartCamera(ActionEvent event) {
+        System.out.println("test");
+
+        if (!this.cameraActive)
+        {
+            // start the video capture
+            this.capture.open(cameraId);
+            
+            // is the video stream available?
+            if (this.capture.isOpened())
+            {
+                this.cameraActive = true;
+                
+                // grab a frame every 33 ms (30 frames/sec)
+                Runnable frameGrabber = () -> {
+                    // effectively grab and process a single frame
+                    Mat frame = grabFrame();
+                    // convert and show the frame
+                    Image imageToShow = Utils.mat2Image(frame);
+                    updateImageView(currentFrame, imageToShow);
+                };
+                
+                this.timer = Executors.newSingleThreadScheduledExecutor();
+                this.timer.scheduleAtFixedRate(frameGrabber, 0, 33, TimeUnit.MILLISECONDS);
+                
+                // update the button content
+                this.button.setText("Stop Camera");
+            }
+            else
+            {
+                // log the error
+                System.err.println("Impossible to open the camera connection...");
+            }
+        }
+        else
+        {
+            // the camera is not active at this point
+            this.cameraActive = false;
+            // update again the button content
+            this.button.setText("Start Camera");
+            
+            // stop the timer
+            this.stopAcquisition();
+        }
+    }
+
+    /**
+	 * Get a frame from the opened video stream (if any)
+	 *
+	 * @return the {@link Mat} to show
+	 */
+	private Mat grabFrame()
+	{
+		// init everything
+		Mat frame = new Mat();
+		
+		// check if the capture is open
+		if (this.capture.isOpened())
+		{
+			try
+			{
+				// read the current frame
+				this.capture.read(frame);
+				
+				// // if the frame is not empty, process it
+				if (!frame.empty())
+				{
+                    // TODO: BL
+					// Imgproc.cvtColor(frame, frame, Imgproc.COLOR_BGR2GRAY);
+				}
+				
+			}
+			catch (Exception e)
+			{
+				// log the error
+				System.err.println("Exception during the image elaboration: " + e);
+			}
+		}
+		
+		return frame;
+	}
+	
+	/**
+	 * Stop the acquisition from the camera and release all the resources
+	 */
+	private void stopAcquisition()
+	{
+		if (this.timer!=null && !this.timer.isShutdown())
+		{
+			try
+			{
+				// stop the timer
+				this.timer.shutdown();
+				this.timer.awaitTermination(33, TimeUnit.MILLISECONDS);
+			}
+			catch (InterruptedException e)
+			{
+				// log any exception
+				System.err.println("Exception in stopping the frame capture, trying to release the camera now... " + e);
+			}
+		}
+		
+		if (this.capture.isOpened())
+		{
+			// release the camera
+			this.capture.release();
+		}
+	}
+	
+	/**
+	 * Update the {@link ImageView} in the JavaFX main thread
+	 * 
+	 * @param view
+	 *            the {@link ImageView} to update
+	 * @param image
+	 *            the {@link Image} to show
+	 */
+	private void updateImageView(ImageView view, Image image)
+	{
+		Utils.onFXThread(view.imageProperty(), image);
+	}
+	
+	/**
+	 * On application close, stop the acquisition from the camera
+	 */
+	protected void setClosed()
+	{
+		this.stopAcquisition();
+	}
+}
